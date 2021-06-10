8cc2f1d67dc82c0cd6db77a0b244b1012f2d1547
Somanyloopholes <73163846+Somanyloopholes@users.noreply.github.com>
2021-06-11 01:31:43 +0530

    final version

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# WDL-Project (Netmedical)
+# netmedical
+An E-commerce like website.
 
-This project contains all the code to set up a netmeds clone (as close as possible)
-
-html, css, bootstrap, javascript
-
-https://netmedical.herokuapp.com/
+To see the website in action head out to https://netmedical.herokuapp.com/
+(PS: You may need to wait for a few seconds to view the website cause of cold starts)
---
+++ netmedsapp/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+class medicine_type(models.Model):
+    medicine_genre = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.medicine_genre}"
+
+class Medicines(models.Model):
+    name = models.CharField(max_length=1000)
+    manufaturer = models.CharField(max_length=1000)
+    price = models.CharField(max_length=1000)
+    quantity = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000)
+    key_benifits = models.CharField(max_length=1000)
+    dosage = models.CharField(max_length=1000)
+    safety_info = models.CharField(max_length=1000)
+    other_info = models.CharField(max_length=1000)
+    # image = models.ImageField(upload_to='images/')
+    image = CloudinaryField('image')
+    genres = models.ManyToManyField(medicine_type, blank=True, null=True, related_name="Medicines")
+
+    def __str__(self):
+        return f"{self.name}"
